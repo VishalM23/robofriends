2c2dfd1decc8a02a7d620970b82b1acaf929c0ed
Vishal M <vishalm2390@gmail.com>
2020-07-09 09:44:26 +0530

    Updates

---
+++ precache-manifest.93b7a54c1481bdc5de48edb3271a9906.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "030862436b0f17e6b83f62aa6e252f14",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d46907e2956d9938a638",
+    "url": "/robofriends/static/css/main.fac65b00.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d46907e2956d9938a638",
+    "url": "/robofriends/static/js/main.e8495b49.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
